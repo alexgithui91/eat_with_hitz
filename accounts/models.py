1f95138245d5014842db73c1b481cb67c6f5f89f
alexgithui91@gmail.com <alexgithui91@gmail.com>
2022-11-04 16:11:22 +0300

    removed imports

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db.models.fields.related import ForeignKey, OneToOneField
-from django.db.models.signals import post_save, pre_save
-from django.dispatch import receiver
 
 # Create your models here.
 class UserManager(BaseUserManager):
@@ -109,23 +107,3 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.email
-
-
-@receiver(post_save, sender=User)
-def post_save_create_profile_receiver(sender, instance, created, **kwargs):
-    print(created)
-    if created:
-        UserProfile.objects.create(user=instance)
-    else:
-        try:
-            profile = UserProfile.objects.get(user=instance)
-            profile.save()
-        except:
-            # Create the user profile if it does not exist
-            UserProfile.objects.create(user=instance)
-            print("Profile not existing, One has now been created")
-
-
-@receiver(pre_save, sender=User)
-def pre_save_profile_receiver(sender, instance, **kwargs):
-    print(instance.username, "this user is being saved")
