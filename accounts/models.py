516a916623d2cd76a2e8b1382f0676cbe2d0e0ce
alexgithui91@gmail.com <alexgithui91@gmail.com>
2022-11-13 07:28:36 +0300

    made phonenumber unique

--- accounts/models.py
+++ accounts/models.py
@@ -73,7 +73,7 @@ class User(AbstractBaseUser):
     last_name = models.CharField(max_length=255)
     username = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255, unique=True)
-    phone_number = models.CharField(max_length=255, blank=True)
+    phone_number = models.CharField(max_length=255, unique=True)
     role = models.PositiveSmallIntegerField(
         choices=ROLE_CHOICE, blank=True, null=True
     )
