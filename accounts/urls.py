00c9620f998af1c461aec840416408a3ee75aabc
alexgithui91@gmail.com <alexgithui91@gmail.com>
2022-11-20 19:23:55 +0300

    working vendor profile page and link

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path("", views.myAccount),
     path("registerUser/", views.registerUser, name="registerUser"),
     path("registerVendor/", views.registerVendor, name="registerVendor"),
     path("login/", views.login, name="login"),
@@ -19,4 +20,5 @@ urlpatterns = [
         name="resetPasswordValidate",
     ),
     path("resetPassword/", views.resetPassword, name="resetPassword"),
+    path("vendor/", include("vendor.urls")),
 ]
