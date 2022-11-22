fe29f7b15f3278efe77656060923bf3daa23336e
alexgithui91@gmail.com <alexgithui91@gmail.com>
2022-11-23 02:46:14 +0300

    manage restaurant form created

--- vendor/views.py
+++ vendor/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .forms import VendorForm
+from accounts.forms import UserProfileForm
 
 # Create your views here.
 def vendor_profile(request):
-    return render(request, "vendor/vendor_profile.html")
+    profile_form = UserProfileForm()
+    vendor_form = VendorForm()
+
+    context = {
+        "profile_form": profile_form,
+        "vendor_form": vendor_form,
+    }
+    return render(request, "vendor/vendor_profile.html", context)
